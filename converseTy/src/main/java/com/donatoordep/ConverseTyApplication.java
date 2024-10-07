578ddc2ad200bcc56371cc01219b9f6ae790babe
zilohnoji <donatoordep@gmail.com>
2024-10-07 09:38:52 -0300

    added menu

--- converseTy/src/main/java/com/donatoordep/ConverseTyApplication.java
+++ converseTy/src/main/java/com/donatoordep/ConverseTyApplication.java
@@ -8,16 +8,44 @@ import com.donatoordep.entities.Currency;
 import com.donatoordep.enums.CurrencyCode;
 import com.donatoordep.mappers.libs.impl.JacksonDeserializer;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
 public class ConverseTyApplication {
 
     private static final JacksonDeserializer<GetPairQuotationResponseDTO> jacksonDeserializer = JacksonDeserializer.createInstance(GetPairQuotationResponseDTO.class);
     private static final ApiExchangeRateSpecification<Currency> service = new ExchangeRateService(jacksonDeserializer);
 
-    public static void main(String[] args) throws Throwable {
-        Currency pairQuotation = service.getPairQuotation(CurrencyCode.BRL, CurrencyCode.USD, 20D);
+    public static void main(String[] args) {
 
-        System.out.println(pairQuotation.getLastUpdate());
-        System.out.println(pairQuotation.getCode());
-        System.out.println(pairQuotation.getConversionRates());
+        Currency pairQuotation;
+        List<CurrencyCode> currencies = Arrays.stream(CurrencyCode.values()).toList();
+
+        try (Scanner sc = new Scanner(System.in)) {
+            IntStream.range(0, currencies.size()).forEach(i -> {
+                System.out.printf("[%s] %s ", i, currencies.get(i));
+
+                if ((i + 1) % 6 == 0) {
+                    System.out.println();
+                }
+            });
+
+            System.out.print("Escolha a moeda que deseja converter: ");
+            int baseChoice = sc.nextInt();
+
+            System.out.print("Qual valor deseja converter? ");
+            double value = sc.nextDouble();
+
+            System.out.print("Para qual moeda você deseja converter? ");
+            int secondaryChoice = sc.nextInt();
+
+            pairQuotation = service.getPairQuotation(currencies.get(baseChoice), currencies.get(secondaryChoice), value);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(pairQuotation.getConversionRates() + " " + pairQuotation.getCode() + " " + pairQuotation.getLastUpdate());
     }
 }
